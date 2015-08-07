bbf41c0bc181326c78ff90cd7c88d9b9ada5ba6a
Matt Palen <matthewmpalen@gmail.com>
2015-08-07 11:01:04 -0400

    Close event loop at end

--- 1_introduction/vulnerable_asyncio.py
+++ 1_introduction/vulnerable_asyncio.py
@@ -77,8 +77,9 @@ def main():
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(asyncio.wait(tasks))
-
     print('[!] Finished spawning banner requests')
+
+    loop.close()
 
 if __name__ == '__main__':
     main()
